3ee580ae609caa2cbab99ae1f4b69f5531945b33
miquelmarco <120406764+miquelmarco@users.noreply.github.com>
2023-08-23 13:28:18 -0400

    querys added

---
+++ assets/scripts/index.js
@@ -0,0 +1,14 @@
+let { createApp } = Vue;
+createApp({
+    data() {
+        return {
+            currentSection: 1
+        }
+    },
+    created() {
+    },
+    methods: {
+    },
+    computed: {
+    }
+}).mount("#app")
